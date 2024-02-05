146883388ab32bf4c6d7d43d215fcd4d202b6c48
opbonachera <obonachera@undav.edu.ar>
2024-02-05 06:49:26 -0300

    []

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ export default function AnimatedTitle() {
       <div style={{height: '100vh'}}>
         <div className=" animate__animated animate__fadeInDown h-100 d-flex flex-column align-items-center justify-content-center">
           <h1 className="display-3 fw-bolder text-center">Frontend Web Developer</h1>
-          <p className="fs-4 w-50 text-center">Welcome! My name is Ornella Bonachera. I am a Frontend Web Developer with a year of experience that also pursues a degree in Informatics Engineering.</p>
+          <p className="fs-4 w-50 text-center">Welcome! My name is Ornella Bonachera. Computer Engineering student and fullstack web developer.</p>
           <button className="btn btn-dark button"> <a className="text-decoration-none text-white" target="blank" href='pdf/cvenglish.pdf' download>Download CV</a></button>
         </div>
       </div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,23 +1,20 @@
 import Image from "next/image"
 import { CardComponent as Props } from "@/interfaces/CardComponent"
-import Link from "next/link";
 
 export const ProjectCard: React.FC<Props> = ({ title, imagePath, description, stack, projectLink, githubLink, bgColor }) =>{
     return(
         <>
-           <div className="col-12 col-lg-4 col-md-4 mx-5 rounded d-flex align-items-center justify-content-center bg-dark text-white">
-                <div className="p-5 d-flex align-items-center flex-column justify-content-center">
-                    <div className="">
-                        <h5 className="fw-bold text-center">{ title }</h5>
-                        <p className="text-centerm-5">{ description }</p>
-                        <div className="row">
-                            {
-                                stack.map(e => <div className="col link-tag">{ e }</div>)
-                            }
-                        </div>
+           <div style={{ height:'40rem'}} className="col-12 col-lg-4 col-md-4 mx-5 rounded d-flex align-items-center justify-content-center bg-dark text-white">
+                <div className="d-flex flex-column align-items-center justify-content-between h-100 p-5">
+                    <div id="title" className="fw-bolder">{ title }</div>
+                    <div id="description" className="text-center">{ description }</div>
+                    {/* <div id="img"><Image src={imagePath} alt={title}></Image></div> */}
+                    <div className="row">
+                        {stack.map(e=><div className="col link-tag">{ e }</div>)}
                     </div>
-                    <div className="col-12 col-md-6 col-lg-6">
-                        
+                    <div>
+                        <Image src="/icons/github.png" className="mx-3" alt="github" width={40} height={40}></Image>
+                        <Image src="/icons/github.png" className="mx-3" alt="github" width={40} height={40}></Image>
                     </div>
                 </div>
            </div>
